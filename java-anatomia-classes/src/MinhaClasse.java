cac228dec8db757c625594a7aaba2d40630adc27
Ana Costa <ana.arturcosta@gmail.com>
2026-05-10 15:17:39 +0100

    Aula sobre classes e projeto com MinhaClasse

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,7 @@
+public class MinhaClasse{
+    // Classe executável -> Tem capacidade de inicializar o programa, tem de ter um método especial - main
+    public static void main (String [] args){
+        // Corpo da classe
+        System.out.println("Olá a todos!"); // Classe system, dá-me um output, printando
+    }
+}
